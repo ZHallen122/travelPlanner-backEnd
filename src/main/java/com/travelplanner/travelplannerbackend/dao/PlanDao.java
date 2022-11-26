ecbee55899631b2505bfa2f3eca33995793f7b5c
ZHallen122 <zhallen12261@gmail.com>
2022-11-25 19:02:52 -0500

    plan dao and SmallerPlanDao finish

--- src/main/java/com/travelplanner/travelplannerbackend/dao/PlanDao.java
+++ src/main/java/com/travelplanner/travelplannerbackend/dao/PlanDao.java
@@ -1,6 +1,8 @@
 package com.travelplanner.travelplannerbackend.dao;
 
+import com.travelplanner.travelplannerbackend.entity.Cart;
 import com.travelplanner.travelplannerbackend.entity.Plan;
+import com.travelplanner.travelplannerbackend.entity.SmallerPlan;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,29 @@ public class PlanDao {
 
             //save to db
             session.save(plan);
+            session.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            if (session != null) {
+                session.getTransaction().rollback();
+            }
+        }finally {
+            if(session!=null) {
+                session.close();
+            }
+        }
+    }
+
+    public void removeSmallerPlan(int sPlanId){
+        Session session = null;
+        try {
+            session=sessionFactory.openSession();
+            SmallerPlan splan = session.get(SmallerPlan.class,sPlanId);
+            Plan plan = splan.getPlan();
+            plan.getListOfSmallerPlan().remove(sPlanId);
+
+            session.beginTransaction();
+            session.delete(splan);
             session.getTransaction().commit();
         } catch (Exception ex) {
             ex.printStackTrace();
--- src/main/java/com/travelplanner/travelplannerbackend/dao/SmallerPlanDao.java
+++ src/main/java/com/travelplanner/travelplannerbackend/dao/SmallerPlanDao.java
@@ -1,7 +1,37 @@
 package com.travelplanner.travelplannerbackend.dao;
 
+import com.travelplanner.travelplannerbackend.entity.Plan;
+import com.travelplanner.travelplannerbackend.entity.SmallerPlan;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class SmallerPlanDao {
-}
+    @Autowired
+    SessionFactory sessionFactory;
+
+    public void saveSmallerPlan(SmallerPlan smallerPlan){
+            Session session = null;
+            try {
+                session = sessionFactory.openSession();
+                session.beginTransaction();
+
+                //save to db
+                session.save(smallerPlan);
+                session.getTransaction().commit();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                if (session != null) {
+                    session.getTransaction().rollback();
+                }
+            }finally {
+                if(session!=null) {
+                    session.close();
+                }
+            }
+        }
+
+    }
+
