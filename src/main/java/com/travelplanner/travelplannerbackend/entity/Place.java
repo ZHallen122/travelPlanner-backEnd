20e4dbdfc7305582e26ef1e89920a537d327663c
ZHallen122 <zhallen12261@gmail.com>
2022-11-27 22:58:00 -0500

    place table done, still need to change

--- src/main/java/com/travelplanner/travelplannerbackend/entity/Place.java
+++ src/main/java/com/travelplanner/travelplannerbackend/entity/Place.java
@@ -2,14 +2,44 @@ package com.travelplanner.travelplannerbackend.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="Place")
+@Table(name="place")
 public class Place {
 
   //!!!!!!!!!!!!!!! Not sure yet dont do this table.
     @Id
     private String place;
 
+    @ManyToOne
+    private PointOfInterest pointOfInterest;
+
+    public PointOfInterest getPointOfInterest() {
+      return pointOfInterest;
+    }
+
+    public void setPointOfInterest(PointOfInterest pointOfInterest) {
+      this.pointOfInterest = pointOfInterest;
+    }
+
+    @ManyToOne
+    private Plan plan;
+
+    public Plan getPlan() {
+      return plan;
+    }
+
+    public void setPlan(Plan plan) {
+      this.plan = plan;
+    }
+
+  public String getPlace() {
+      return place;
+    }
+
+    public void setPlace(String place) {
+      this.place = place;
+    }
 }
