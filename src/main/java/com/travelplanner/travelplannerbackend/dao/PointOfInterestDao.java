070d41739f889bd15460c90b2e5d4d5bef62c585
Qinlin Wu <103500621+qinlinwu@users.noreply.github.com>
2022-11-30 20:11:19 +0100

    Update PointOfInterestDao.java

--- src/main/java/com/travelplanner/travelplannerbackend/dao/PointOfInterestDao.java
+++ src/main/java/com/travelplanner/travelplannerbackend/dao/PointOfInterestDao.java
@@ -1,10 +1,43 @@
 package com.travelplanner.travelplannerbackend.dao;
 
+import com.travelplanner.travelplannerbackend.entity.PointOfInterest;
+import com.travelplanner.travelplannerbackend.entity.SmallerPlan;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.ArrayList;
+import java.util.List;
+
 //@Repository
 public class PointOfInterestDao {
+    @Autowired
+    private SessionFactory sessionFactory;
+    public List<PointOfInterest> getAllPointOfInterest() {
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<PointOfInterest> criteria = builder.createQuery(Restaurant.class);
+            criteria.from(PointOfInterest.class);
+            return session.createQuery(criteria).getResultList();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return new ArrayList<>();
+    }
+    public void savePointOfInterest(int smallerPlanId, int PointOfInterestId) {
+        try (Session session = sessionFactory.openSession()) {
+            SmallerPlan smallerPlan = session.get(PointOfInterest.class, smallerPlanId);
+            if (smallerPlan != null) {
+                smallerPlan.savePointOfInterest(PointOfInterestId);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
 
 }
