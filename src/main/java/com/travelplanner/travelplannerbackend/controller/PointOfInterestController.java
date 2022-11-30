fbfd8a7a51df697c8697ad04c54a93fd8723b1d9
Qinlin Wu <103500621+qinlinwu@users.noreply.github.com>
2022-11-30 23:42:55 +0100

    Add files via upload

---
+++ src/main/java/com/travelplanner/travelplannerbackend/controller/PointOfInterestController.java
@@ -0,0 +1,27 @@
+package com.travelplanner.travelplannerbackend.controller;
+
+import com.travelplanner.travelplannerbackend.entity.PointOfInterest;
+import com.travelplanner.travelplannerbackend.service.PointOfInterestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public class PointOfInterestController {
+    @Autowired
+    private PointOfInterestService pointOfInterestService;
+
+    @RequestMapping(value = "/pointOfInterest", method = RequestMethod.GET)
+    @ResponseBody
+    public List<PointOfInterest> getAllPointOfInterest() {
+        return pointOfInterestService.getAllPointOfInterest();
+    }
+
+    @RequestMapping(value = "/{planId}/{smallerPlanId}/addPointOfInterest/{pointOfInterestID}", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void savePointOfInterest(@PathVariable int planId,@PathVariable int smallerPlanId,@PathVariable int pointOfInterestID) {
+        pointOfInterestService.savePointOfInterest( planId,smallerPlanId,pointOfInterestID);
+    }
+}
